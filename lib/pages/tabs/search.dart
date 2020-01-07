9224e7b218724b8ed5550799b4315fbe7e1972d2
yinjiangqaq <1139277782@qq.com>
2020-01-07 17:11:11 +0800

    update devlin

--- lib/pages/tabs/search.dart
+++ lib/pages/tabs/search.dart
@@ -5,18 +5,61 @@ import 'package:flutter/material.dart';
 class SearchPage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return CupertinoPageScaffold(
+    return Scaffold(
         // child:Center(
         //     child:Text('发现',
         //       style: Theme.of(context).textTheme.button,)
         // )
-     child:ListView(
-       children: <Widget>[
-         Row(
-           
-         )
-       ],
+    resizeToAvoidBottomPadding: false,
+    body: Container(
+      height: 150,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/searchBack.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+     child: Center(
+          child: Padding(
+              padding: EdgeInsets.all(20),
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Row(
+                children: <Widget>[
+                Expanded(
+                            flex: 5,
+                            child: TextField(
+                                style: TextStyle(fontSize: 30),
+                                decoration: InputDecoration.collapsed(
+                                    border: InputBorder.none,
+                                    hintText: "搜索地址",
+                                    hintStyle: TextStyle())),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: IconButton(
+                              icon: Icon(Icons.search),
+                                padding:EdgeInsets.all(10.0),
+                              iconSize: 30,
+                              
+                              onPressed: (){
+
+                                print("aa");
+                              },
+                                color: Colors.blueAccent,
+                                highlightColor: Colors.black
+                            ),
+                          )
+              ],),
+              )
+          ],
+        ),
+      ),
+    ),
      )
+    )
     );
   }
 }
