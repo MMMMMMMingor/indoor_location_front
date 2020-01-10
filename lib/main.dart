742be680b7697770d8a6762c294ee5d519c2259a
LT201730682508 <1419544834@qq.com>
2020-01-10 16:48:40 +0800

    命名路由

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,37 @@ import 'dart:io';
 import 'pages/tabs.dart';
 import 'pages/Go.dart';
 void main() => runApp(MyApp());
-
 class MyApp extends StatelessWidget {
+  final routes= {
+    '/Go': (context) => Go(),
+
+    // '/search':(context,{arguments})=>SearchPage(arguments:arguments),
+  };
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData.light(),
         home: MyPage(),
-      routes: {
-      '/Go':(context)=>Go()
-      },
+       onGenerateRoute :(RouteSettings settings) {
+      // 统一处理
+      final String name = settings.name;
+      final Function pageContentBuilder = routes[name];
+      if (pageContentBuilder != null) {
+        if (settings.arguments != null) {
+          final Route route = MaterialPageRoute(
+              builder: (context) =>
+                  pageContentBuilder(context, arguments: settings.arguments));
+          return route;
+        }else{
+          final Route route = MaterialPageRoute(
+              builder: (context) =>
+                  pageContentBuilder(context));
+          return route;
+        }
+      }return null;
+    }
+
     );
   }
 }
